623a751afad6e1149e7ff365c54c2ba592bc629a
Marcos Costa <64797599+marcos-vcg@users.noreply.github.com>
2020-08-28 20:59:34 -0300

    Exercício2

---
+++ Desafio/src/exercicios/E2.java
@@ -0,0 +1,20 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class E2 {
+
+	public static void main(String[] args) {
+		System.out.println("Informe a palavra:");
+		Scanner leitor = new Scanner(System.in);
+		String entrada = leitor.next();
+		String saida = "";
+		
+		for (int i = entrada.length() - 1; i >= 0; i--) {
+			saida = saida + entrada.charAt(i);
+		}
+		System.out.println("A palavra invertida é:");
+		System.out.println(saida);
+	}
+	
+}
