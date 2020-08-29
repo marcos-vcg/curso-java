06b8d85b05edab106b3943ea44e31c190c3fa4aa
Marcos Costa <64797599+marcos-vcg@users.noreply.github.com>
2020-08-29 14:20:44 -0300

    Update E4.java

--- Desafio/src/exercicios/E4.java
+++ Desafio/src/exercicios/E4.java
@@ -3,6 +3,7 @@ package exercicios;
 public class E4 {
 
 	public static void main(String[] args) {
+		
 		String frase = "teste 1 de 2 string 3";
 		frase = frase.replace("1", "[removido]");
 		frase = frase.replace("2", "[removido]");
