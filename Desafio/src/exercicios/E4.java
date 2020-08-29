94e360f73ffece1221dbd3f1bea1d5602442e658
Marcos Costa <64797599+marcos-vcg@users.noreply.github.com>
2020-08-29 11:25:41 -0300

    exercicio4

---
+++ Desafio/src/exercicios/E4.java
@@ -0,0 +1,19 @@
+package exercicios;
+
+public class E4 {
+
+	public static void main(String[] args) {
+		String frase = "teste 1 de 2 string 3";
+		frase = frase.replace("1", "[removido]");
+		frase = frase.replace("2", "[removido]");
+		frase = frase.replace("3", "[removido]");
+		System.out.println(frase);
+		
+	}
+
+	private static void replace(String string, String string2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
