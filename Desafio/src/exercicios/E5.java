8e761fc8331c88115725a958c42232702b44d742
Marcos Costa <64797599+marcos-vcg@users.noreply.github.com>
2020-08-30 20:29:15 -0300

    exercicio5b

---
+++ Desafio/src/exercicios/E5.java
@@ -0,0 +1,24 @@
+package exercicios;
+
+import java.util.Map;
+
+public class E5 {
+
+	public static void main(String[] args) {
+
+		Map<String, String> dicionario = new HashMap<>();
+		dicionario.put("4", new String("a"));
+		dicionario.put("3", new String("e"));
+		dicionario.put("1", new String("i"));
+		dicionario.put("5", new String("s"));
+		
+		dicionario = {4: 'a', 3: 'e', 1: 'i', 5: 's'};
+		String frase = "T35t3 d3 35t4g1o";
+		
+		for (int i = 0; frase.length; i++) {
+			if (char[i] =  
+		}
+
+	}
+
+}
---
+++ Desafio/src/exercicios/E5b.java
@@ -0,0 +1,15 @@
+package exercicios;
+
+public class E5b {
+
+	
+		
+	public static void main(String[] args) {
+		
+		String frase = "T35t3 d3 35t4g1o";
+		frase = (((frase.replace("4", "a")).replace("3", "e")).replace("1", "i")).replace("5", "s");	
+
+		System.out.println(frase);
+	}
+	
+}
