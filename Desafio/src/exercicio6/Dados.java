a05141b9d7cf9015496aac55b1ed5eb8ffac3de4
Marcos Costa <64797599+marcos-vcg@users.noreply.github.com>
2020-09-01 15:56:15 -0300

    atualizacaoE6

--- Desafio/src/exercicio6/Dados.java
+++ Desafio/src/exercicio6/Dados.java
@@ -7,11 +7,33 @@ public class Dados {
 		Cadastro[] pessoa = new Cadastro[4];
 		
 		pessoa[0] = new Cadastro(1, "jose", "da silva", 42);
-		pessoa[1] = new Cadastro(4, "andre", "silva", 40);
-		pessoa[2] = new Cadastro(2, "joao", "da silva", 20);
-		pessoa[3] = new Cadastro(3, "luiz", "marcos", 30);
+		pessoa[1] = new Cadastro(4, "joão", "gonçalves", 21);
+		pessoa[2] = new Cadastro(2, "pedro", "garcia", 28);
+		pessoa[3] = new Cadastro(3, "matheus", "pinheiro", 21);
 		
-		System.out.println(pessoa);
+		for (int i = 0; i<pessoa.length; i++) {			
+			System.out.println(new StringBuilder().append("Olá, ").append(PrimeiraMaiuscula(pessoa[i].getNome())) + " " + pessoa[i].getSobrenome());
+			System.out.println("Olá, " + PrimeiraMaiuscula(pessoa[i].getNome()) + " " + pessoa[i].getSobrenome());
+		}
+		
+		
+		
+//		public static String[] Saudacao(Person[] pPersons) {
+//			
+//			String[] msgs = new String[pPersons.length];
+//			for (Integer i = 0; i < pPersons.length; i++) {
+//				pPersons[i].firstName = pPersons[i].firstName.substring(0, 1).toUpperCase() + 
+//						pPersons[i].firstName.substring(1);
+//				msgs[i] = "Olá " + (pPersons[i].firstName) + " " + pPersons[i].lastName;
+//			}
+//			
+//			return msgs;
+//		}
+		
 	}
-
+	
+	public static String PrimeiraMaiuscula(String palavra) {
+		return palavra = palavra.substring(0,1).toUpperCase().concat(palavra.substring(1));
+	}
+	
 }
--- Desafio/src/exercicio6/E6a.java
+++ Desafio/src/exercicio6/E6a.java
@@ -12,12 +12,7 @@ public class E6a {
 		String [] lista = new String[4];
 		lista[0] = new Dados;
 		
-			lista = [
-			         {id: 1, nome: "josé", sobrenome: "da silva", idade: 42},
-			         {id: 2, nome: 'joão', sobrenome: 'gonçalves',  idade: 21},
-			         {id:3, nome: 'pedro', sobrenome: 'garcia', idade: 28},
-			         {id: 4, nome: 'matheus', sobrenome: 'pinheiro',  idade: 21}
-			         ]
+			
 
 		System.out.println("Olá, " + cadastro.get(nome).chartAt(0).toUpperCase());
 			
