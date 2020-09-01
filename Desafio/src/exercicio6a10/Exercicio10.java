b3e7d7942119cea5c413e08bac146da4acbb5805
Marcos Costa <64797599+marcos-vcg@users.noreply.github.com>
2020-09-01 19:06:46 -0300

    exercicio10

--- Desafio/src/exercicio6a10/Exercicio10.java
+++ Desafio/src/exercicio6a10/Exercicio10.java
@@ -1,5 +1,14 @@
 package exercicio6a10;
 
 public class Exercicio10 {
-
+	// E.10 Ordene o array de forma decrescente por idade, em caso de empate o desempate é pelo id.
+	public static void main(String[] args) {
+		
+		Cadastro[] pessoa = new Cadastro[4];
+		pessoa[0] = new Cadastro(1, "jose", "da silva", 42);
+		pessoa[1] = new Cadastro(2, "joão", "gonçalves", 21);
+		pessoa[2] = new Cadastro(3, "pedro", "garcia", 28);
+		pessoa[3] = new Cadastro(4, "matheus", "pinheiro", 21);
+		
+	}
 }
