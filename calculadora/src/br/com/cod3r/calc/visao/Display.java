dfb1109218a342d53ba24cbd108b5b0deabfa9f9
Marcos Costa <64797599+marcos-vcg@users.noreply.github.com>
2020-09-23 11:21:40 -0300

    Display

--- calculadora/src/br/com/cod3r/calc/visao/Display.java
+++ calculadora/src/br/com/cod3r/calc/visao/Display.java
@@ -1,13 +1,25 @@
 package br.com.cod3r.calc.visao;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Display extends JPanel {
 
+	private final JLabel label;
+	
 	public Display() {
-		setBackground(Color.GREEN);
+		setBackground(new Color(46, 49, 50));
+		label = new JLabel("1234,56");									// Informação Textual
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("courier", Font.PLAIN, 30));             // Font
+		
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));        	// Layout alinhado a direita e posições
+		
+		add(label);
 	}
 }
